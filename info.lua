075560fe51499a342b1be346f1c0f6f91726d82e
MansfieldTX-MattR <matt.reid@mansfieldtexas.gov>
2026-03-03 14:55:17 -0600

    Move Color and PrettyName into `PluginInfo`

--- info.lua
+++ info.lua
@@ -1,8 +1,10 @@
 PluginInfo = {
     Name = "Base Plugin~A Test",
+    PrettyName = "Base Plugin",
     Version = "0.0",
     BuildVersion = "0.0.0.0",
     Id = "<guid>",
     Author = "",
     Description = "Compiled from Visual Studio Code",
+    Color = { 102, 102, 102 },
 }
